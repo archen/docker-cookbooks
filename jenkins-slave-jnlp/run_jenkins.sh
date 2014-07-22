566523e750c147d29b403fe4799b57a46f58797c
Kevin Glavin <archen.sol@gmail.com>
2014-07-22 18:03:48 -0400

    added example script to run one master and one slave

--- jenkins-slave-jnlp/run_jenkins.sh
+++ jenkins-slave-jnlp/run_jenkins.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # run the master
-# run the number of slaves specified in argument 1
+docker run -d --name jenkins-master -v /data/jenkins-master:/jenkins -p 8080:8080 -p 8081:8081 localhost:5000/jenkins-master
+docker run --link jenkins-master:jenkins -d -v /data/jenkins-slave-jnlp-$1/envs:/etc/container_environment -v /data/jenkins-slave-jnlp-$1/jenkins:/var/lib/jenkins --name jenkins-slave-jnlp-$1 localhost:5000/jenkins-slave-jnlp
