0aae01f19678c06998b470721e53961a27c6a7c0
archen <archen.sol@gmail.com>
2014-11-20 12:55:48 -0500

    reconfigured to allow drop-in build of elasticsearch.yml

--- elasticsearch/elasticsearch.sh
+++ elasticsearch/elasticsearch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 exec 2>$1
-exec /elasticsearch/bin/elasticsearch -Des.config=/data/elasticsearch.yml
+exec /elasticsearch/bin/elasticsearch -Des.config=/data/elasticsearch/elasticsearch.yml
